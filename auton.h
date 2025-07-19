148a3002370d27036a2576166429575ef332ecb7
AdiAsh1729 <adiash1729@gmail.com>
2025-07-19 06:17:20 -0700

    Create auton.h

---
+++ auton.h
@@ -0,0 +1,18 @@
+#ifndef AUTON_H
+#define AUTON_H
+
+void turn(double);
+void slowturn(double);
+void drive(std::string, double);
+void slowdrive(std::string, double);
+void DrivePID_Test();
+void TurnPID_Test();
+void AWPRed();
+void AWPBlue();
+void Red();
+void Blue();
+void GoalRushRed();
+void GoalRushBlue();
+void AutonSkills();
+
+#endif
