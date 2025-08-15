8c4e08746e3919515fe1071f8f9489d542ac1a3f
AdiAsh1729 <adiash1729@gmail.com>
2025-08-14 22:19:44 -0700

    Update robot-config.hpp

--- robot-config.hpp
+++ robot-config.hpp
@@ -1,4 +1,5 @@
 #pragma once
+#include "vex.h"
 using namespace vex;
 
 extern brain Brain;
@@ -12,5 +13,14 @@ extern motor FR;
 extern motor MR;
 extern motor BR;
 extern motor_group RightDrive;
+
+extern motor Store;
+extern motor Intake1;
+extern motor Intake2;
+extern motor Intake3;
+
+extern digital_out PL;
+
+extern inertial Inertial;
 
 void vexcodeInit(void);
