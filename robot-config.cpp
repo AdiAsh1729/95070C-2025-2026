4d4646101db927e07140238934c7e1c23b08816d
AdiAsh1729 <adiash1729@gmail.com>
2025-08-14 22:19:28 -0700

    Update robot-config.cpp

--- robot-config.cpp
+++ robot-config.cpp
@@ -9,13 +9,22 @@ brain Brain;
 controller Controller;
 
 
-motor FL = motor(PORT2, ratio6_1, true);
-motor ML = motor(PORT1, ratio6_1, false);
-motor BL = motor(PORT10, ratio6_1, true);
+motor FL = motor(PORT1, ratio6_1, false);
+motor ML = motor(PORT3, ratio6_1, false);
+motor BL = motor(PORT15, ratio6_1, false);
 motor_group LeftDrive = motor_group(FL, ML, BL);
-motor FR = motor(PORT8, ratio6_1, false);
+motor FR = motor(PORT16, ratio6_1, true);
 motor MR = motor(PORT19, ratio6_1, true);
-motor BR = motor(PORT4, ratio6_1, false);
+motor BR = motor(PORT17, ratio6_1, true);
 motor_group RightDrive = motor_group(FR, MR, BR);
+
+motor Store = motor(PORT18, ratio6_1, true);
+motor Intake1 = motor(PORT14, ratio18_1, false);
+motor Intake2 = motor(PORT10, ratio18_1, false);
+motor Intake3 = motor(PORT11, ratio18_1, false);
+
+digital_out PL = digital_out(Brain.ThreeWirePort.A);
+
+inertial Inertial = inertial(PORT11);
 
 void vexcodeInit(void);
