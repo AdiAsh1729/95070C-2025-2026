59c0c115fba3fe86fbd40082affe860d3d546576
legoninja2014 <colinkshou@gmail.com>
2025-09-23 16:49:20 -0700

    Update robot-config.cpp

--- robot-config.cpp
+++ robot-config.cpp
@@ -9,25 +9,26 @@ brain Brain;
 controller Controller;
 
 
-motor FL = motor(PORT4, ratio6_1, true);
-motor ML = motor(PORT7, ratio6_1, true);
-motor BL = motor(PORT2, ratio6_1, true);
+motor FL = motor(PORT10, ratio6_1, true);
+motor ML = motor(PORT14, ratio6_1, true);
+motor BL = motor(PORT13, ratio6_1, true);
 motor_group LeftDrive = motor_group(FL, ML, BL);
-motor FR = motor(PORT5, ratio6_1, false);
-motor MR = motor(PORT6, ratio6_1, false);
-motor BR = motor(PORT3, ratio6_1, false);
+motor FR = motor(PORT1, ratio6_1, false);
+motor MR = motor(PORT17, ratio6_1, false);
+motor BR = motor(PORT18, ratio6_1, false);
 motor_group RightDrive = motor_group(FR, MR, BR);
 
-motor Intake1 = motor(PORT1, ratio18_1, true);
-motor Intake2 = motor(PORT16, ratio18_1, false);
+motor Intake1 = motor(PORT8, ratio18_1, true);
+motor Intake2 = motor(PORT11, ratio18_1, false);
 motor_group Intake = motor_group(Intake1, Intake2);
 
-digital_out StickyStickyBoom = digital_out(Brain.ThreeWirePort.C);
 digital_out Park1 = digital_out(Brain.ThreeWirePort.B);
-//digital_out Park2 = digital_out(Brain.ThreeWirePort.B);
-digital_out MiddleGoal = digital_out(Brain.ThreeWirePort.A);
+digital_out Park2 = digital_out(Brain.ThreeWirePort.B);
+digital_out MP = digital_out(Brain.ThreeWirePort.H);
+digital_out Load = digital_out(Brain.ThreeWirePort.C);
+digital_out Bunny = digital_out(Brain.ThreeWirePort.H);
 
-inertial Inertial = inertial(PORT8);
+inertial Inertial = inertial(PORT16);
 rotation Rotation = rotation(PORT22, true);
 
 void vexcodeInit(void);
