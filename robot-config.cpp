caf8d3643feaab7a4629ef45f1f3285ec2222533
legoninja2014 <colinkshou@gmail.com>
2025-09-26 15:20:26 -0700

    Update robot-config.cpp

--- robot-config.cpp
+++ robot-config.cpp
@@ -22,11 +22,11 @@ motor Intake1 = motor(PORT8, ratio18_1, true);
 motor Intake2 = motor(PORT11, ratio18_1, false);
 motor_group Intake = motor_group(Intake1, Intake2);
 
-digital_out Park1 = digital_out(Brain.ThreeWirePort.B);
-digital_out Park2 = digital_out(Brain.ThreeWirePort.B);
+digital_out Park1 = digital_out(Brain.ThreeWirePort.A);
+digital_out Park2 = digital_out(Brain.ThreeWirePort.A);
 digital_out MP = digital_out(Brain.ThreeWirePort.H);
-digital_out Load = digital_out(Brain.ThreeWirePort.C);
-digital_out Bunny = digital_out(Brain.ThreeWirePort.H);
+digital_out Load = digital_out(Brain.ThreeWirePort.B);
+digital_out Bunny = digital_out(Brain.ThreeWirePort.G);
 
 inertial Inertial = inertial(PORT16);
 rotation Rotation = rotation(PORT22, true);
